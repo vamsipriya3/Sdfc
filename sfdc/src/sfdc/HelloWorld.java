83d1357e7e71559656c52b432727bee3e4a69820
vamsi <vamsi@192.168.0.7>
2019-02-22 11:53:08 -0600

    Good BYE world got created

--- sfdc/src/sfdc/HelloWorld.java
+++ sfdc/src/sfdc/HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World");
+		System.out.println("Hello World ");
+		System.out.println("BY World");
 	}
+	
 
 }
