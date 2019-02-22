e8494bf81e28512b412118781b711d2f7dbff1db
vamsi <vamsi@192.168.0.7>
2019-02-22 11:35:32 -0600

    Hello world got created

---
+++ sfdc/src/sfdc/HelloClass.java
@@ -0,0 +1,10 @@
+package sfdc;
+
+public class HelloClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Class..!");
+	}
+
+}
---
+++ sfdc/src/sfdc/HelloWorld.java
@@ -0,0 +1,10 @@
+package sfdc;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+	}
+
+}
